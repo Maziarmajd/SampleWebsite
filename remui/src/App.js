cb2b1a226bb6f6801e8d722209839b263f804274
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2022-06-11 15:17:34 +0100

    Create Title

--- remui/src/App.js
+++ remui/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "@mui/material";
+import { useEffect } from 'react';
 
 
 function App() {
+  document.title= "React Material UI - Home";
+useEffect(() => {
+
+}, []);
+
   return (
     <div className="App">
-
+      <Button>test</Button>
 
     </div>
   );
